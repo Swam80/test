ae4ccf11337e42bc4d5f949023941c7987f9419c
unknown <swamesh80@gmail.com>
2023-01-31 22:03:35 +0530

    Soda

--- hungry.py
+++ hungry.py
@@ -3,5 +3,11 @@ hungry = input(" Are you hungry ?")
 if hungry == 'Ye':
     print('Eat Pizza')
 else:
-    print(' OKAY')
+    thirsty = input("are you thirsty ?")
+    if thirsty == 'Y':
+        print(" Have Soda")
+    else:
+        print(' Get lsot')
+
+
     
